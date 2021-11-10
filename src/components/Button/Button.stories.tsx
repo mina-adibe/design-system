c14cb773720f9dfa1cbf71a503a1ab829fe133ae
alexn400 <alexn400@gmail.com>
2021-11-10 15:13:43 +1300

    Fix scripts

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,17 +1,17 @@
-import Button, { ButtonProps } from "./Button";
-import {Meta, Story} from '@storybook/react';
+import Button, { ButtonProps } from './Button';
+import { Meta, Story } from '@storybook/react';
 
 export default {
   component: Button,
-  title: "Components/Form/Custom Button",
+  title: 'Components/Form/Custom Button',
   argTypes: {
-    backgroundColor: { control: "color" },
+    backgroundColor: { control: 'color' },
   },
 } as Meta;
 
-const Template: Story<ButtonProps> = (args) => <Button {...args} />;
+let Template: Story<ButtonProps> = (args) => <Button {...args} />;
 
-// Reuse that template for creating different stories
+//Reuse that template for creating different stories
 export const Primary = Template.bind({});
 Primary.args = { text: 'Primary button!' };
 
