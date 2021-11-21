ae95295ad1c016dbdf4db496211e2d00782395dc
alexn400 <alexn400@gmail.com>
2021-11-21 22:42:40 +1300

    add export for map component

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,7 +3,9 @@ export * from './form';
 export { default as AspectImage } from './AspectImage/AspectImage';
 export { default as ReservedBanner } from './ReservedBanner/ReservedBanner';
 export { default as Hero } from './Hero/Hero';
+export { default as MapView } from './MapView/MapView';
 
 export type { AspectImageProps } from './AspectImage/AspectImage';
 export type { ReservedBannerProps } from './ReservedBanner/ReservedBanner';
 export type { HeroProps } from './Hero/Hero';
+export type { MapViewProps } from './MapView/MapView';
